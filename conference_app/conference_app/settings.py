f1b07f52782b6f6235a7e1d609a4eebbdd1bb679
ValentasV <rroebd@gmail.com>
2023-06-08 18:40:55 +0300

    keitimas

--- conference_app/conference_app/settings.py
+++ conference_app/conference_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = True if os.getenv("SECRET_KEY")=="True" else False
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
